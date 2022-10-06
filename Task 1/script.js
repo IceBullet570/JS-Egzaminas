01deaf24fae205174b7ec21271df020301424225
IceBullet570 <109753557+IceBullet570@users.noreply.github.com>
2022-10-06 21:08:52 +0300

    v0.07

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,10 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+
+function weightConverter(value) {
+    document.getElementById("outputPounds").innerHTML=value*2.2046;
+    document.getElementById("outputGrams").innerHTML=value/0.001;
+    document.getElementById("outputOz").innerHTML=value*35.274;
+  }
