4039a46e08f8fbb805f11a1948da61ecaf6f68bc
IceBullet570 <109753557+IceBullet570@users.noreply.github.com>
2022-10-08 21:15:11 +0300

    v0.2

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,21 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+
+let firstNumber = prompt("Pirmas skaicius?")
+let secondNumber = prompt("Antras skaicius?")
+
+function Calculator(firstNumber, secondNumber) {
+    function sum(){
+        firstNumber + secondNumber
+    }
+    // this.first = firstNumber
+    // this.second = secondNumber
+    // this.suma = function sum() {
+    //     return (
+    //     first + second)
+    // }
+}
+Calculator(firstNumber, secondNumber)
+console.log(Calculator(firstNumber, secondNumber))
