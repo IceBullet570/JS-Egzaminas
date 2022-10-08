50930aaafd2baf8f4f78b94eab4667add0703809
IceBullet570 <109753557+IceBullet570@users.noreply.github.com>
2022-10-08 18:49:17 +0300

    v1.0

--- Task 7/script.js
+++ Task 7/script.js
@@ -10,3 +10,12 @@ const audi = {
   year: 2005,
   color: 'white',
 };
+
+// console.log(Object.keys(audi))
+
+function showObjectKeys(audi){
+  console.log(Object.keys(audi))
+}
+showObjectKeys(audi)
+
+// finished
